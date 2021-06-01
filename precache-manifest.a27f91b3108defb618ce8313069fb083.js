f4c64a34418a67c03aa98f28841116ee5e149206
Aneessaifi1 <aneessaifi88020@gmail.com>
2021-06-01 16:27:27 +0530

    Updates

---
+++ precache-manifest.a27f91b3108defb618ce8313069fb083.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d4fbadfd8e8f612791022fede458d4",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "80017defb8fa08e34490",
+    "url": "/react-gh-pages/static/css/main.2a8442e3.chunk.css"
+  },
+  {
+    "revision": "a5d0ff6c478aa5b433aa",
+    "url": "/react-gh-pages/static/js/2.755c874a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-gh-pages/static/js/2.755c874a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80017defb8fa08e34490",
+    "url": "/react-gh-pages/static/js/main.5b79475f.chunk.js"
+  },
+  {
+    "revision": "1caab8dc5f48a18416c9",
+    "url": "/react-gh-pages/static/js/runtime-main.08eec64a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
